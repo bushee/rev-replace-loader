25c32070aed58737d9c8a0b3308241c036cd9c72
Krzysztof "Bushee" Nowaczyk <bushee01@gmail.com>
2017-11-03 14:34:33 +0100

    reformatted code

--- index.js
+++ index.js
@@ -3,34 +3,36 @@ var glob = require('glob');
 var loaderUtils = require('loader-utils');
 
 module.exports = function (source) {
-  var query = loaderUtils.parseQuery(this.query);
-  var manifest = query.manifest || (query.manifestPath ? requireManifests(query.manifestPath) : false);
+    var query = loaderUtils.parseQuery(this.query);
+    var manifest = query.manifest || (query.manifestPath ? requireManifests(query.manifestPath) : false);
 
-  if (manifest) {
-    for (var key in manifest) {
-      var value = manifest[key];
-      var pattern = new RegExp(key, 'gm');
-      source = source.replace(pattern, value);
+    if (manifest) {
+        for (var key in manifest) {
+            if (manifest.hasOwnProperty(key)) {
+                var value = manifest[key];
+                var pattern = new RegExp(key, 'gm');
+                source = source.replace(pattern, value);
+            }
+        }
     }
-  }
 
-  return source;
+    return source;
 };
 
 function requireManifests(manifestGlob) {
-  return glob.sync(manifestGlob).map(function (path) {
-    return requireManifest(path);
-  }).filter(function (manifest) {
-    return manifest !== false;
-  }).reduce(function (manifest, current) {
-    return Object.assign(manifest, current);
-  }, {});
+    return glob.sync(manifestGlob).map(function (path) {
+        return requireManifest(path);
+    }).filter(function (manifest) {
+        return manifest !== false;
+    }).reduce(function (manifest, current) {
+        return Object.assign(manifest, current);
+    }, {});
 }
 
 function requireManifest(manifestPath) {
-  try {
-    return require(path.resolve(process.cwd(), manifestPath));
-  } catch (error) {
-    return false;
-  }
+    try {
+        return require(path.resolve(process.cwd(), manifestPath));
+    } catch (error) {
+        return false;
+    }
 }
